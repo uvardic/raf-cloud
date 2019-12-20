d932283ccc68cbc4661dfff71a34e690f1fceff6
uvardic <uros.vardiic@gmail.com>
2019-12-20 15:16:20 +0100

    Initial commit

---
+++ src/main/java/njp/raf/cloud/RafCloudApplication.java
@@ -0,0 +1,13 @@
+package njp.raf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RafCloudApplication.class, args);
+    }
+
+}
