45ee1ee845037fdf92ecd4b0769fa57b39618288
uvardic <uros.vardiic@gmail.com>
2019-12-20 17:09:59 +0100

    Cleanup

--- src/main/java/njp/raf/cloud/user/domain/User.java
+++ src/main/java/njp/raf/cloud/user/domain/User.java
@@ -26,22 +26,10 @@ public class User {
     @NotNull
     private String password;
 
-    @Size(min = MIN_FIRST_NAME_SIZE, max = MAX_FIRST_NAME_SIZE)
+    @Size(min = 2, max = 100)
     private String firstName;
 
-    @Size(min = MIN_LAST_NAME_SIZE, max = MAX_LAST_NAME_SIZE)
+    @Size(min = 2, max = 100)
     private String lastName;
-
-    public static final int MIN_USERNAME_SIZE = 5;
-
-    public static final int MAX_USERNAME_SIZE = 100;
-
-    public static final int MIN_FIRST_NAME_SIZE = 2;
-
-    public static final int MAX_FIRST_NAME_SIZE = 100;
-
-    public static final int MIN_LAST_NAME_SIZE = 2;
-
-    public static final int MAX_LAST_NAME_SIZE = 100;
 
 }
--- src/main/java/njp/raf/cloud/user/service/UserPreparationService.java
+++ src/main/java/njp/raf/cloud/user/service/UserPreparationService.java
@@ -14,29 +14,6 @@ public class UserPreparationService {
 
     private static final Pattern BCRYPT_PATTERN = Pattern.compile("^\\$2[ayb]\\$.{56}$");
 
-    private static final String USERNAME_SIZE_ERROR_MESSAGE;
-
-    private static final String FIRST_NAME_SIZE_ERROR_MESSAGE;
-
-    private static final String LAST_NAME_SIZE_ERROR_MESSAGE;
-
-    static {
-        USERNAME_SIZE_ERROR_MESSAGE = String.format(
-                "Invalid username! Username size must be between %d and %d!",
-                User.MIN_USERNAME_SIZE, User.MAX_USERNAME_SIZE
-        );
-
-        FIRST_NAME_SIZE_ERROR_MESSAGE = String.format(
-                "Invalid first name! First name size must be between %d and %d!",
-                User.MIN_FIRST_NAME_SIZE, User.MAX_FIRST_NAME_SIZE
-        );
-
-        LAST_NAME_SIZE_ERROR_MESSAGE = String.format(
-                "Invalid last name! Last name size must be between %d and %d!",
-                User.MIN_LAST_NAME_SIZE, User.MAX_LAST_NAME_SIZE
-        );
-    }
-
     private final UserRepository userRepository;
 
     public UserPreparationService(UserRepository userRepository) {
@@ -58,8 +35,6 @@ public class UserPreparationService {
         if (usernameExists(user.getUsername()))
             throw new InvalidUserInfoException(String.format("Username: %s, already exists!", user.getUsername()));
 
-//        validateUserAttributeSize(user);
-
         user.setPassword(hashPassword(user.getPassword()));
 
         return user;
@@ -70,7 +45,8 @@ public class UserPreparationService {
     }
 
     public User prepareUserForUpdate(Long existingId, User user) {
-//        validateUserAttributeSize(user);
+        if (isUsernameChanged(existingId, user.getUsername()) && usernameExists(user.getUsername()))
+            throw new InvalidUserInfoException(String.format("Username: %s, already exists!", user.getUsername()));
 
         if (isPasswordPlain(user.getPassword()))
             user.setPassword(hashPassword(user.getPassword()));
@@ -80,35 +56,10 @@ public class UserPreparationService {
         return user;
     }
 
-    private void validateUserAttributeSize(User user) {
-        if (isUsernameSizeInvalid(user.getUsername()))
-            throw new InvalidUserInfoException(USERNAME_SIZE_ERROR_MESSAGE);
-
-        if (isFirstNameSizeInvalid(user.getFirstName()))
-            throw new InvalidUserInfoException(FIRST_NAME_SIZE_ERROR_MESSAGE);
-
-        if (isLastNameSizeInvalid(user.getLastName()))
-            throw new InvalidUserInfoException(LAST_NAME_SIZE_ERROR_MESSAGE);
-    }
-
-    private boolean isUsernameSizeInvalid(String username) {
-        return username.length() < User.MIN_USERNAME_SIZE || username.length() > User.MAX_USERNAME_SIZE;
-    }
-
-    private boolean isFirstNameSizeInvalid(String firstName) {
-        // First name is nullable so we let it slide if its null
-        if (firstName == null)
-            return false;
-
-        return firstName.length() < User.MIN_FIRST_NAME_SIZE || firstName.length() > User.MAX_FIRST_NAME_SIZE;
-    }
-
-    private boolean isLastNameSizeInvalid(String lastName) {
-        // Last name is nullable so we let it slide if its null
-        if (lastName == null)
-            return false;
-
-        return lastName.length() < User.MIN_LAST_NAME_SIZE || lastName.length() > User.MAX_LAST_NAME_SIZE;
+    private boolean isUsernameChanged(Long existingId, String newUsername) {
+        return !userRepository.findById(existingId)
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with id: %d not found!", existingId)))
+                .getUsername().equals(newUsername);
     }
 
     private boolean isPasswordPlain(String password) {
